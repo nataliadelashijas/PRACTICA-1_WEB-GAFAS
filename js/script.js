ba1622cec5455b2c956341eb6e107a3bde96f156
natalia <ndelashijas@gmail.com>
2023-11-02 17:56:05 +0100

    carrusel

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 console.log ("a ver si esto funciona")
 
-// PAGINA CARGA (JQuery)
-$(document).ready(function() {
-    setTimeout(function() {
-        $(".loader").fadeOut(1000);
-    },1100);
-});
+// PANTALLA DE CARGA
+function loading() {
+    "use strict";
+  
+    setTimeout(function () {
+        $("#carga").toggleClass("desaparecer");
+    }, 2000);
+  
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("myslides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+
+  slides[slideIndex-1].style.display = "block";  
+
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
