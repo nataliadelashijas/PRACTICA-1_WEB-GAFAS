c951981aafd7a6c17c49d5da15c88a6b1d291ac3
maria <meryaparicio12@gmail.com>
2023-11-02 14:50:13 +0100

    carga

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log ("a ver si esto funciona")
+
+// PAGINA CARGA (JQuery)
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".loader").fadeOut(1000);
+    },1100);
+});
+
